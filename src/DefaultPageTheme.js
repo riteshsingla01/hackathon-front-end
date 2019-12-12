f76a313f76ca15e7a0b26adf5bb9bd5dde66b557
michael bowman <michael.bowman.contractor@stjude.org>
2019-12-12 10:37:35 -0600

    Created search page skeleton

---
+++ src/DefaultPageTheme.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import createMuiTheme from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: #d11947
+    },
+    secondary: {
+      main: #ffffff
+    }
+  }
+})
+
+export default theme;
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,9 +1,64 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import './DefaultPageStyle.css'
 
 function SearchPage()
 {
   return (
-    <p>test</p>
+    <div className='page'>
+      <Grid container>
+        <Grid container spacing={2} justify='left'>
+          <Grid item>
+            <TextField id='first-name' label='First Name' />
+          </Grid>
+          <Grid item>
+            <TextField id='middle-name' label='Middle Name' />
+          </Grid>
+          <Grid item>
+            <TextField id='last-name' label='Last Name' />
+          </Grid>
+          <Grid item style={{ flexGrow: 2 }}></Grid>
+          <Grid item>
+            <TextField id='constituent-id' label='Constituent ID' />
+          </Grid>
+        </Grid>
+        <Grid container style={{ marginTop: '2rem' }} spacing={2} justify='left'>
+          <Grid item>
+            <TextField id='street1' label='Address' />
+          </Grid>
+          <Grid item>
+            <TextField id='street2' label='Address 2' />
+          </Grid>
+          <Grid item>
+            <TextField id='city' label='City' />
+          </Grid>
+          <Grid item>
+            <TextField id='state' label='State' />
+          </Grid>
+          <Grid item>
+            <TextField id='zip' label='Zip Code' />
+          </Grid>
+        </Grid>
+        <Grid container style={{ marginTop: '2rem' }} spacing={2} justify='left'>
+          <Grid item>
+            <TextField id='email' label='Email' />
+          </Grid>
+          <Grid item>
+            <TextField id='phone' label='Phone Number' />
+          </Grid>
+        </Grid>
+        <Grid container style={{ marginTop: '2rem' }} spacing={2} justify='left'>
+          <Grid item>
+            <Button variant='contained'>Search</Button>
+          </Grid>
+          <Grid item>
+            <Button variant='contained'>Clear</Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
   )
 }
 
